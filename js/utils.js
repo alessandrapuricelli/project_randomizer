e3233aafad8d6f3ffbaa7e3737d1301a3ecf5f78
aleß <alessandrapuricelli@gmail.com>
2019-08-26 19:35:05 +0200

    fix moving title

--- js/utils.js
+++ js/utils.js
@@ -137,7 +137,7 @@ function make_colored_spans(string) {
 
 // animate title
 
-let title = new MovingTitle("RANDOM-POSTER-GENERATOR-", 300, 8);
+let title = new MovingTitle("RANDOM-POSTER-GENERATOR-", 300, 20);
 title.init();
 
 
@@ -174,39 +174,3 @@ function MovingTitle(writeText, interval, visibleLetters) {
 
     return _instance;
 }
-
-// var rev = "fwd";
-// function titlebar(val) {
-//     var msg = "RANDOM POSTER GENERATOR ";
-//     var res = " ";
-//     var speed = 100;
-//     var pos = val;
-//     msg = "|-" + msg + "-|";
-//     var le = msg.length;
-//     if (rev == "fwd") {
-//         if (pos < le) {
-//             pos = pos + 1;
-//             scroll = msg.substr(0, pos);
-//             document.title = scroll;
-//             timer = window.setTimeout("titlebar(" + pos + ")", speed);
-//         }
-//         else {
-//             rev = "bwd";
-//             timer = window.setTimeout("titlebar(" + pos + ")", speed);
-//         }
-//     }
-//     else {
-//         if (pos > 0) {
-//             pos = pos - 1;
-//             var ale = le - pos;
-//             scrol = msg.substr(ale, le);
-//             document.title = scrol;
-//             timer = window.setTimeout("titlebar(" + pos + ")", speed);
-//         }
-//         else {
-//             rev = "fwd";
-//             timer = window.setTimeout("titlebar(" + pos + ")", speed);
-//         }
-//     }
-// }
-// titlebar(0);
